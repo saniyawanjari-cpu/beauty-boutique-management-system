63c52737ef93b01525117613709c644aaadaf5af
Saniya Wanjari <saniyawanjari764@gmail.com>
2026-05-26 11:04:15 +0530

    working dashboard and login setup

--- src/main/java/com/saniya/beautyboutique/controller/HomeController.java
+++ src/main/java/com/saniya/beautyboutique/controller/HomeController.java
@@ -1,8 +1,13 @@
 
 package com.saniya.beautyboutique.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.saniya.beautyboutique.entity.Customer;
 import com.saniya.beautyboutique.repository.CustomerRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,6 +19,67 @@ public class HomeController {
     @Autowired
     CustomerRepository repo;
 
+    @GetMapping("/dashboard")
+    public String dashboard(Model model) {
+
+        List<Customer> customers = repo.findAll();
+
+        double totalRevenue = 0;
+
+        for(Customer c : customers) {
+
+            totalRevenue += Double.parseDouble(c.getAmount());
+
+        }
+
+        model.addAttribute(
+                "customers",
+                customers
+        );
+
+        model.addAttribute(
+                "totalBookings",
+                customers.size()
+        );
+
+        model.addAttribute(
+                "totalRevenue",
+                totalRevenue
+        );
+
+        return "dashboard";
+    }
+
+    @GetMapping("/login")
+    public String loginPage() {
+
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(
+
+            @RequestParam String username,
+            @RequestParam String password,
+            Model model
+
+    ) {
+
+        if(username.equals("admin")
+                &&
+                password.equals("Glow@2026")) {
+
+            return "redirect:/dashboard";
+        }
+
+        model.addAttribute(
+                "error",
+                "Invalid Username or Password"
+        );
+
+        return "login";
+    }
+
     @GetMapping("/")
     public String home(Model model) {
 
@@ -22,10 +88,12 @@ public class HomeController {
         double totalRevenue = 0;
 
         for(Customer c : repo.findAll()) {
+
             totalRevenue += Double.parseDouble(c.getAmount());
         }
 
         model.addAttribute("totalBookings", totalBookings);
+
         model.addAttribute("totalRevenue", totalRevenue);
 
         return "index";
